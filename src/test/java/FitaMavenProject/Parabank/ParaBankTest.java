1e4f8bba6125600e37dd6114aaaf278fcdbb2c17
AmuthaKarthi <amutha.ame.k@gmail.com>
2025-10-01 22:47:50 +0800

    Msg deleted

--- src/test/java/FitaMavenProject/Parabank/ParaBankTest.java
+++ src/test/java/FitaMavenProject/Parabank/ParaBankTest.java
@@ -52,13 +52,7 @@ public class ParaBankTest extends BaseTest {
         System.out.println("Statement Summary (Second Account): " + accountsPage.getStatementSummary());
         System.out.println("Thank you Poomani");
         System.out.println("Thank you Rahul shetty");
-        System.out.println("Thank you Udamy");
-        System.out.println("Thank you Fita");
-        System.out.println("Thank you Poomani");
-        System.out.println("Thank you Rahul shetty");
-        System.out.println("Thank you Udamy");
-        System.out.println("Thank you Fita");
-
+        
     }
 }
 
